db11c9aa204d08993fb83cabc2e1b2e66fab81d9
BayramovNicat <bayramov.nicat@hotmail.com>
2016-02-01 17:59:27 +0400

    Chat

--- js/common.js
+++ js/common.js
@@ -16,21 +16,66 @@ function sidebar_submenu_click(){
 		return false;
 	});
 }
+function chat_open(){
+	$(".chat-btn").on('click', function(){
+		$('.side-chat').addClass('active');
+		var tl = new TimelineMax();
+		tl.to($('.side-chat'), 0.3, {width:"335px",ease:Back.easeInOut})
+			.to($('.side-chat .loader'), 0.3, {opacity:1, visibility:'visible'})
+			.to($('.search-input'), .3, {bottom: 20, opacity: 1,visibility: 'visible'},"+=0.6");
+		
+		// settimeoutnan userleri yukleyirem
+		setTimeout(function() {
+			TweenMax.to($('#user-list'),0.3, {opacity:1, visibility:'visible'});
+			TweenMax.to($('.side-chat .loader'), 0.3, {opacity:0,visibility:'hidden'});
+		}, 1000);
+	});
+	$('.user-panel .actions .fa-times').on('click', function(){
+		$('.side-chat').removeClass('active');
+		$('#user-list, #message-list').css({'opacity':'0','visibility':'hidden'});
+		$('.search-input,.message-input').css({'opacity':'0.5','bottom':'0','visibility':'hidden'});
+		TweenMax.to($('.side-chat'), 0.3, {width:"0px",ease:Back.easeInOut});
+	});
+}
 $(document).ready(function(){
 	sidebar_width();
 	sidebar_submenu_init();
 	sidebar_submenu_click();
-	$(".chat-btn").on('click', function(){
-		if($('.side-chat').hasClass('active')){
-			TweenMax.to($('.side-chat'), 0.3, {width:"0px",ease:Back.easeInOut});
-		}else{
-			TweenMax.to($('.side-chat'), 0.3, {width:"335px",ease:Back.easeInOut});
-		}
-		$('.side-chat').toggleClass('active');
+	chat_open();
+
+	$('#user-list .list-group a.list-group-item').on('click', function(){
+		var tl = new TimelineMax();
+		tl.to($('#user-list'), 0.3, {opacity:0, visibility:'hidden'})
+			.to($('.side-chat .loader'), 0.3, {opacity:1, visibility:'visible'})
+			.to($('#message-list'), 0.3, {opacity:1, visibility:'visible'})
+			.to($('.side-chat .loader'), 0.3, {opacity:0,visibility:'hidden'})
+			.to($('.message-input'), .3, {bottom: 20, opacity: 1,visibility: 'visible'},"-=0.3");
+			$('.search-input').css({'opacity':'0.5','bottom':'0','visibility':'hidden'});
 	});
-	$('.user-panel .actions .fa-times').on('click', function(){
-		$('.side-chat').toggleClass('active');
+
+	$('#message-list a.chat-back').on('click', function(){
+		var tl = new TimelineMax();
+		tl.to($('#message-list'), 0.3, {opacity:0, visibility:'hidden'})
+			.to($('.side-chat .loader'), 0.3, {opacity:1, visibility:'visible'})
+			.to($('#user-list'), 0.3, {opacity:1, visibility:'visible'})
+			.to($('.side-chat .loader'), 0.3, {opacity:0,visibility:'hidden'})
+			.to($('.search-input'), .3, {bottom: 20, opacity: 1,visibility: 'visible'},"-=0.3");
+			$('.message-input').css({'opacity':'0.5','bottom':'0','visibility':'hidden'});
 	});
+
+	$('#message-form').submit(function(){
+		var html = '<div class="message-dialog" style="opacity: 0; margin-top: 110px">'+
+                        '<div class="message-text right">'+
+                            '<p>'+$(this).children('input').val()+'</p>'+
+                        '</div>'+
+                    '</div>';
+		$('#message-list .list-group').append(html);
+		TweenMax.to($('#message-list .list-group .message-dialog:last-child'), 0.3, {opacity:1, marginTop: 10, ease:Back.easeInOut});
+		
+		$(this).children('input').val('');
+		return false;
+	});
+
 });
 
 $( window ).resize(function() {
